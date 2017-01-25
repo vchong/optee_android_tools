48fc734814362a1ed88c5dd263b81c2e5b9e1061
Victor Chong <victor.chong@linaro.org>
2017-01-25 20:25:30 +0000

    Include copy to hikey-kernel/ cmds

--- build_kernel.sh
+++ build_kernel.sh
@@ -12,6 +12,7 @@ if [ -z "$ANDROID_BUILD_TOP" ]; then
    echo "  $ lunch <target>"
 fi
 
+echo "ANDROID_PRODUCT_OUT=$ANDROID_PRODUCT_OUT"
 dest="${ANDROID_PRODUCT_OUT}/kernel_obj"
 
 cross=optee/gcc-linaro-4.9-2015.05-x86_64_aarch64-linux-gnu/bin
@@ -27,7 +28,26 @@ kerndir=device/linaro/hikey-kernel
 
 CPU_CORES=$(nproc)
 
-flags="CROSS_COMPILE=${cross} ARCH=arm64 -j${CPU_CORES} O=${dest}"
+#flags="CROSS_COMPILE=${cross} ARCH=arm64 -j${CPU_CORES} O=${dest}"
+flags="CROSS_COMPILE=${cross} ARCH=arm64 -j${CPU_CORES}"
 
 make -C $kerndir ${flags} hikey_defconfig || die "Unable to configure kernel"
 make -C $kerndir ${flags} || die "Unable to build kernel"
+echo "cp $kerndir/arch/arm64/boot/dts/hisilicon/hi6220-hikey.dtb $kerndir/hi6220-hikey.dtb-4.9"
+cp $kerndir/arch/arm64/boot/dts/hisilicon/hi6220-hikey.dtb $kerndir/hi6220-hikey.dtb-4.9
+echo "cp $kerndir/arch/arm64/boot/Image-dtb $kerndir/Image-dtb-4.9"
+cp $kerndir/arch/arm64/boot/Image-dtb $kerndir/Image-dtb-4.9
+
+# if 'make bootimage' is run before a complete build is successful then we
+# might be missing other files required to make the bootimage?
+# http://source.android.com/source/devices.html#running-android-hikey
+# Building the kernel
+#make bootimage -j${CPU_CORES}
+# this will create boot.img in ${ANDROID_PRODUCT_OUT}
+# i.e. optee_android_manifest/out/target/product/hikey
+# neither
+#make bootimage -j${CPU_CORES} TARGET_BOOTIMAGE_USE_FAT=true
+# nor
+#TARGET_BOOTIMAGE_USE_FAT=true make bootimage -j${CPU_CORES}
+# create boot_fat.uefi.img
+# have to rerun 'make all' to create it
